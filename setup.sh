49505cd9ebb632c8164bbf771cf1ced2d9b7e3ed
Meekail Zain <zainmeekail@gmail.com>
2023-11-21 10:38:12 -0500

    Attempt at self-restarting script

--- setup.sh
+++ setup.sh
@@ -7,25 +7,20 @@ if [ $# -eq 0 ]
     sudo apt update
     sudo apt-get install git -y
 
-    "${SHELL}" <(curl -L micro.mamba.pm/install.sh) << START_ARGS
-
-    y
-    y
-
-    START_ARGS
+    "${SHELL}" <(curl -L micro.mamba.pm/install.sh) <<< "\ny\ny\n\n"
     exec "$startup.sh" dummy_arg
+else
+    source ~/.bashrc
+    micromamba create -n flax python~=3.10.0 <<< "y"
+    micromamba activate flax
+    pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+    micromamba install gh
+
+    git clone https://github.com/micky774/flax
+    cd flax
+    pip install -e ".[all]"
+    pip install -e ".[testing]"
+    pip install -r docs/requirements.txt
+    pip install pre-commit
+    pre-commit install
 fi
-
-source ~/.bashrc
-micromamba create -n flax python~=3.10.0 <<< "y"
-micromamba activate flax
-pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-micromamba install gh
-
-git clone https://github.com/micky774/flax
-cd flax
-pip install -e ".[all]"
-pip install -e ".[testing]"
-pip install -r docs/requirements.txt
-pip install pre-commit
-pre-commit install
