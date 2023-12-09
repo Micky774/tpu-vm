f2896cd2776a0b271651013a53ff09c79d85f8cb
Meekail Zain <zainmeekail@gmail.com>
2023-12-09 14:40:13 -0500

    Added missing wget

--- xla_setup.sh
+++ xla_setup.sh
@@ -4,6 +4,7 @@ cd ~
 mkdir bin
 wget https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64 -o bin/bazel
 chmod +x bin/bazel
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt update
 sudo apt install cuda-toolkit-12-3 libcudnn8-dev
