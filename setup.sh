6d23c157a1ab1fa0f65c9f7b6bc416d37f2e38cc
Meekail Zain <zainmeekail@gmail.com>
2023-12-11 14:02:20 -0500

    Update

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@
 # . ./setup.sh
 #-----------------------------
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo apt update
 sudo apt-get install git -y
 
--- xla_setup.sh
+++ xla_setup.sh
@@ -2,11 +2,11 @@
 
 cd ~
 mkdir bin
-wget https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64 -o bin/bazel
+wget https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64 -O bin/bazel
 chmod +x bin/bazel
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt update
-sudo apt install cuda-toolkit-12-3 libcudnn8-dev
+sudo apt install -y cuda-toolkit-12-3 libcudnn8-dev
 git clone https://github.com/Micky774/xla.git
 cd xla
