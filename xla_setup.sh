bc733f08153f49107830c20aa8015eda192d4198
Meekail Zain <zainmeekail@gmail.com>
2023-12-11 14:20:51 -0500

    Updated bazel install path

--- xla_setup.sh
+++ xla_setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd ~
-mkdir bin
-wget https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64 -O bin/bazel
-chmod +x bin/bazel
+wget https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64 -O /usr/local/bin/bazel
+chmod +x /usr/local/bin/bazel
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt update
